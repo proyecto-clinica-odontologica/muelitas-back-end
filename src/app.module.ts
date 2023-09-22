992b8b57e7a34f5af21dee8befebd64ea38b50ec
Maycol R. <maycol.rodriguez.ma@gmail.com>
2023-09-22 01:37:37 -0500

    recurso completo de autenticacion agregado

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JoiValidationSchema } from './config/joi.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { JoiValidationSchema } from './config/joi.config';
       synchronize: true,
       autoLoadEntities: true,
     }),
+    AuthModule,
   ],
   controllers: [],
   providers: [],
