7b8d6c9a06fa1c066c75784bca472591550958c7
Maycol R. <maycol.rodriguez.ma@gmail.com>
2023-09-22 01:30:21 -0500

    filtro de excepciones a las solicitudes - rojo

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExceptionsFilter } from './common/filters/exceptions/exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
+  app.useGlobalFilters(new ExceptionsFilter());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
