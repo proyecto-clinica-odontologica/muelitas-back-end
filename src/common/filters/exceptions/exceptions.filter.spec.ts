8f6d19af82c8e9803ca5bc5f5043b7d619cfbd70
Maycol R. <maycol.rodriguez.ma@gmail.com>
2023-09-22 01:31:28 -0500

    filtro de excepciones a las solicitudes - verde

--- src/common/filters/exceptions/exceptions.filter.spec.ts
+++ src/common/filters/exceptions/exceptions.filter.spec.ts
@@ -1,7 +1,65 @@
+import { ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
 import { ExceptionsFilter } from './exceptions.filter';
 
 describe('ExceptionsFilter', () => {
-  it('should be defined', () => {
-    expect(new ExceptionsFilter()).toBeDefined();
+  let filter: ExceptionsFilter<unknown>;
+
+  beforeEach(() => {
+    filter = new ExceptionsFilter();
+  });
+
+  it('debe definirse', () => {
+    expect(filter).toBeDefined();
+  });
+
+  it('debe devolver la respuesta correcta para una excepción HttpException', () => {
+    const mockResponse = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    const mockHost = {
+      switchToHttp: jest.fn().mockReturnThis(),
+      getRequest: jest.fn().mockReturnValue({ url: '/test' }),
+      getResponse: jest.fn().mockReturnValue(mockResponse),
+    };
+    const mockException = new HttpException(
+      'Test message',
+      HttpStatus.BAD_REQUEST,
+    );
+
+    filter.catch(mockException, mockHost as unknown as ArgumentsHost);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(HttpStatus.BAD_REQUEST);
+    expect(mockResponse.json).toHaveBeenCalledWith({
+      statusCode: HttpStatus.BAD_REQUEST,
+      timestamp: expect.any(String),
+      path: '/test',
+      message: 'Test message',
+    });
+  });
+
+  it('debe devolver la respuesta correcta para una excepción que no sea HttpException', () => {
+    const mockResponse = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    const mockHost = {
+      switchToHttp: jest.fn().mockReturnThis(),
+      getRequest: jest.fn().mockReturnValue({ url: '/test' }),
+      getResponse: jest.fn().mockReturnValue(mockResponse),
+    };
+    const mockException = new Error('Test message');
+
+    filter.catch(mockException, mockHost as unknown as ArgumentsHost);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+    expect(mockResponse.json).toHaveBeenCalledWith({
+      statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+      timestamp: expect.any(String),
+      path: '/test',
+      message: mockException,
+    });
   });
 });
