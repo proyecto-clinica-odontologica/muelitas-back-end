9f4bd59af06b82d11ac3b91bc08279b450e852bb
Maycol R. <maycol.rodriguez.ma@gmail.com>
2023-09-22 19:04:37 -0500

    cambio de mensaje de dtos mas detallado

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -7,19 +7,22 @@ import {
 } from 'class-validator';
 
 export class CreateUserDto {
-  @IsString()
-  @IsEmail()
+  @IsEmail({}, { message: 'El correo debe ser un correo electrónico valido' })
   email: string;
 
-  @IsString()
-  @MinLength(6)
-  @MaxLength(50)
+  @IsString({ message: 'La contraseña debe ser un texto' })
+  @MinLength(6, {
+    message: 'La contraseña debe tener como minimo 6 caracteres',
+  })
+  @MaxLength(50, { message: 'la contraseña debe tener menos de 50 caracteres' })
   @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
     message: 'La contraseña debe tener mayúsculas, minúsculas y un número',
   })
   password: string;
 
-  @IsString()
-  @MinLength(3)
+  @IsString({ message: 'El nombre completo debe ser un texto' })
+  @MinLength(3, {
+    message: 'El nombre completo debe tener como minimo 3 caracteres',
+  })
   fullName: string;
 }
