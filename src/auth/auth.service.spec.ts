ba06c08325888f4dee8419d3fd205868e258ebcd
Maycol R. <maycol.rodriguez.ma@gmail.com>
2023-09-28 00:46:59 -0500

    test al servicio de autenticación - verde

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,66 +1,52 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from './auth.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Repository } from "typeorm";
+import { AuthService } from "./auth.service";
+import { User } from "./entities/user.entity";
+import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { CreateUserDto } from "./dto/create-user.dto";
+
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let userRepositoryMock: Partial<jest.Mocked<Repository<User>>>;
 
   beforeEach(async () => {
+    userRepositoryMock = {
+      create: jest.fn().mockImplementation((user) => user),
+      save: jest.fn().mockResolvedValue(undefined),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: userRepositoryMock,
+        },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
   });
 
   describe('create', () => {
-    it('debe crear una nueva autenticación', () => {
-      const createAuthDto: CreateUserDto = {
-        email: 'test@test.com',
-        fullName: 'testuser',
-        password: 'testpassword',
+    it('crea un usuario', async () => {
+      const createUserDto: CreateUserDto = {
+        correo: 'ejemplo@ejemplo.com',
+        contra: 'Contra1234!',
+        numDocumento: '12345678',
+        nombre: 'juan',
+        apellido: 'perez',
       };
-      expect(service.create(createAuthDto)).toEqual(
-        'This action adds a new auth',
-      );
-    });
-  });
 
-  describe('findAll', () => {
-    it('debe devolver todas la autenticaciónes registradas', () => {
-      expect(service.findAll()).toEqual('This action returns all auth');
-    });
-  });
+      await authService.create(createUserDto);
 
-  describe('findOne', () => {
-    it('debe devolver una autenticación específica', () => {
-      const id = 1;
-      expect(service.findOne(id)).toEqual(`This action returns a #${id} auth`);
-    });
-  });
-
-  describe('update', () => {
-    it('debe actualizar una autenticación específica', () => {
-      const id = 1;
-      const updateAuthDto: UpdateUserDto = {
-        fullName: 'testuser',
-        password: 'testpassword',
-      };
-      expect(service.update(id, updateAuthDto)).toEqual(
-        `This action updates a #${id} auth`,
-      );
-    });
-  });
-
-  describe('remove', () => {
-    it('debe eliminar una autenticación específica', () => {
-      const id = 1;
-      expect(service.remove(id)).toEqual(`This action removes a #${id} auth`);
+      expect(userRepositoryMock.create).toBeCalled(); // Simplemente verifica que fue llamada
+      expect(userRepositoryMock.save).toBeCalled();
     });
   });
 });
