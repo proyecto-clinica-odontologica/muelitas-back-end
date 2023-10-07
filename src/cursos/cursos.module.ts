f87c861f4800d82b3f8f1be9c63d019a1f4142b7
Maycol R. <maycol.rodriguez.ma@gmail.com>
2023-10-07 00:24:50 -0500

    modulo de cursos actualizado

--- src/cursos/cursos.module.ts
+++ src/cursos/cursos.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { CursosService } from './cursos.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CursosController } from './cursos.controller';
+import { CursosService } from './cursos.service';
+import { Curso } from './entities/curso.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Curso])],
   controllers: [CursosController],
   providers: [CursosService],
+  exports: [TypeOrmModule, CursosService],
 })
 export class CursosModule {}
