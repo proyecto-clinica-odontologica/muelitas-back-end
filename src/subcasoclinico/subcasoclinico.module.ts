06d061b4dfd8a09e74788ebcfc76abf37c0c6766
Carlos-all <carlitosjhonatan17@gmail.com>
2023-10-19 18:30:28 -0500

    module imports

--- src/subcasoclinico/subcasoclinico.module.ts
+++ src/subcasoclinico/subcasoclinico.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SubcasoclinicoController } from './subcasoclinico.controller';
 import { SubcasoclinicoService } from './subcasoclinico.service';
+import { SubCasoClinico } from './entities/subcasoclinico.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([SubCasoClinico])],
   controllers: [SubcasoclinicoController],
-  providers: [SubcasoclinicoService]
+  providers: [SubcasoclinicoService],
+  exports: [TypeOrmModule, SubcasoclinicoService],
 })
 export class SubcasoclinicoModule {}
+
