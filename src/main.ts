3909c87e92aebe44eea5a0b6fbeb035ecaee5e0c
Maycol R. <maycol.rodriguez.ma@gmail.com>
2023-10-22 01:27:38 -0500

    antes de fusionar con rama remota jh

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     }),
   );
   await app.listen(process.env.PORT);
-  console.log(`Server on port ${process.env.PORT}`);
+  console.log(`Server on port ${process.env.PORT} 🚀`);
 }
 
 bootstrap();
